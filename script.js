9a2ddceaaa49630e647c1fa444497ccf594ab160
Oleg <ddosmotr@gmail.com>
2017-09-04 00:11:48 +0300

    add countergit status and test work with file.txt for future projet about git

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
 (function(){
+  //--------------------------------------
+  //--------------------------------------
+  //Ура, счетчик, нормааааас. Замыкания))))
+  let func = function(){
+    let count = 1;
+    return function(){
+      //count++;
+      console.log(++count);
+    }
+  };
+  //var timerId = setInterval(func(), 2000);
+  //---------------------------------------
+  //---------------------------------------
+
+  // 1. Создаём новый объект XMLHttpRequest
+  var xhr = new XMLHttpRequest();
+
+  // 2. Конфигурируем его: GET-запрос на URL 'phones.json'
+  xhr.open('POST', ' test.txt', false);
+
+  // 3. Отсылаем запрос
+  xhr.send();
+
+  // 4. Если код ответа сервера не 200, то это ошибка
+  if (xhr.status != 200) {
+    // обработать ошибку
+    alert( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
+  } else {
+    // вывести результат
+    console.log( xhr.responseText ); // responseText -- текст ответа.
+  }
+
   var app = new Vue({
       el: '#test',
       data: {
